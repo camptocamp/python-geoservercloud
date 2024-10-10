283be30fce77e85304def4894ec54cb985ed77cd
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2024-10-10 09:15:13 +0200

    Add user and role management

--- tests/test_role.py
+++ tests/test_role.py
@@ -56,3 +56,46 @@ def test_delete_role(geoserver: GeoServerCloud) -> None:
         response = geoserver.delete_role(role)
 
         assert response.status_code == 200
+
+
+def test_get_user_roles(geoserver: GeoServerCloud) -> None:
+    user = "test_user"
+    roles = ["test_role1", "test_role2"]
+    with responses.RequestsMock() as rsps:
+        rsps.get(
+            url=f"{geoserver.url}/rest/security/roles/user/{user}.json",
+            status=200,
+            json={"roles": roles},
+        )
+
+        response = geoserver.get_user_roles(user)
+
+        assert response == roles
+
+
+def test_assign_role_to_user(geoserver: GeoServerCloud) -> None:
+    user = "test_user"
+    role = "test_role"
+    with responses.RequestsMock() as rsps:
+        rsps.post(
+            url=f"{geoserver.url}/rest/security/roles/role/{role}/user/{user}",
+            status=200,
+        )
+
+        response = geoserver.assign_role_to_user(user, role)
+
+        assert response.status_code == 200
+
+
+def test_remove_role_from_user(geoserver: GeoServerCloud) -> None:
+    user = "test_user"
+    role = "test_role"
+    with responses.RequestsMock() as rsps:
+        rsps.delete(
+            url=f"{geoserver.url}/rest/security/roles/role/{role}/user/{user}",
+            status=200,
+        )
+
+        response = geoserver.remove_role_from_user(user, role)
+
+        assert response.status_code == 200
---
+++ tests/test_user.py
@@ -0,0 +1,74 @@
+import responses
+import responses.matchers
+
+from geoservercloud.geoservercloud import GeoServerCloud
+
+TEST_USER = "test_user"
+
+
+def test_create_user(geoserver: GeoServerCloud) -> None:
+    with responses.RequestsMock() as rsps:
+        rsps.post(
+            url=f"{geoserver.url}/rest/security/usergroup/users",
+            status=201,
+            match=[
+                responses.matchers.json_params_matcher(
+                    {
+                        "user": {
+                            "userName": TEST_USER,
+                            "password": "test_password",
+                            "enabled": True,
+                        }
+                    }
+                )
+            ],
+        )
+        response = geoserver.create_user(TEST_USER, "test_password")
+        assert response.status_code == 201
+
+
+def test_update_user_password(geoserver: GeoServerCloud) -> None:
+    with responses.RequestsMock() as rsps:
+        rsps.post(
+            url=f"{geoserver.url}/rest/security/usergroup/user/{TEST_USER}",
+            status=200,
+            match=[
+                responses.matchers.json_params_matcher(
+                    {
+                        "user": {
+                            "password": "new_password",
+                        }
+                    }
+                )
+            ],
+        )
+        response = geoserver.update_user(TEST_USER, password="new_password")
+        assert response.status_code == 200
+
+
+def test_update_user_enabled(geoserver: GeoServerCloud) -> None:
+    with responses.RequestsMock() as rsps:
+        rsps.post(
+            url=f"{geoserver.url}/rest/security/usergroup/user/{TEST_USER}",
+            status=200,
+            match=[
+                responses.matchers.json_params_matcher(
+                    {
+                        "user": {
+                            "enabled": False,
+                        }
+                    }
+                )
+            ],
+        )
+        response = geoserver.update_user(TEST_USER, enabled=False)
+        assert response.status_code == 200
+
+
+def test_delete_user(geoserver: GeoServerCloud) -> None:
+    with responses.RequestsMock() as rsps:
+        rsps.delete(
+            url=f"{geoserver.url}/rest/security/usergroup/user/{TEST_USER}", status=200
+        )
+        response = geoserver.delete_user(TEST_USER)
+        assert response.status_code == 200
